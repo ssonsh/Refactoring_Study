dd3a841f1f6092c1e6f73af74d607edb193463a9
ssh1224@midasin.com <ssh1224@midasin.com>
2022-03-23 07:41:32 +0900

    리팩토링 - 변수 이름 변경하기

--- src/main/java/com/ssonsh/refactoringstudy/_01_smell_mysterious_name/_01_before/StudyDashboard.java
+++ src/main/java/com/ssonsh/refactoringstudy/_01_smell_mysterious_name/_01_before/StudyDashboard.java
@@ -25,10 +25,10 @@ public class StudyDashboard {
         GHRepository repository = gitHub.getRepository("whiteship/live-study");
         GHIssue issue = repository.getIssue(30);
 
-        List<GHIssueComment> comments = issue.getComments();
-        for (GHIssueComment comment : comments) {
-            usernames.add(comment.getUserName());
-            reviews.add(comment.getBody());
+        List<GHIssueComment> reviews = issue.getComments();
+        for (GHIssueComment reivew : reviews) {
+            usernames.add(reivew.getUserName());
+            this.reviews.add(reivew.getBody());
         }
     }
 
@@ -43,7 +43,7 @@ public class StudyDashboard {
     public static void main(String[] args) throws IOException {
         StudyDashboard studyDashboard = new StudyDashboard();
         studyDashboard.loadReviews();
-        studyDashboard.getUsernames().forEach(System.out::println);
-        studyDashboard.getReviews().forEach(System.out::println);
+        studyDashboard.getUsernames().forEach(name -> System.out.println(name));
+        studyDashboard.getReviews().forEach(review -> System.out.println(review));
     }
 }
