15bcafa1da7177dbc6d15e7ca886b00a75938379
ssh1224@midasin.com <ssh1224@midasin.com>
2022-03-24 07:41:38 +0900

    리팩토링 - 긴 함수
    - 함수를 명령으로 바꾸기

--- src/main/java/com/ssonsh/refactoringstudy/_03_long_function/StudyDashboard.java
+++ src/main/java/com/ssonsh/refactoringstudy/_03_long_function/StudyDashboard.java
@@ -47,16 +47,7 @@ public class StudyDashboard {
                         List<GHIssueComment> comments = issue.getComments();
 
                         for (GHIssueComment comment : comments) {
-                            String username = comment.getUserName();
-                            boolean isNewUser = participants.stream().noneMatch(p -> p.username().equals(username));
-                            Participant participant = null;
-                            if (isNewUser) {
-                                participant = new Participant(username);
-                                participants.add(participant);
-                            } else {
-                                participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
-                            }
-
+                            Participant participant = findParticipant(comment.getUserName(), participants);
                             participant.setHomeworkDone(eventId);
                         }
 
@@ -71,56 +62,19 @@ public class StudyDashboard {
         latch.await();
         service.shutdown();
 
-        try (FileWriter fileWriter = new FileWriter("participants.md");
-             PrintWriter writer = new PrintWriter(fileWriter)) {
-            participants.sort(Comparator.comparing(Participant::username));
+        new StudyPrinter(this.totalNumberOfEvents, participants).exeute();
+    }
 
-            writer.print(header(participants.size()));
-
-            participants.forEach(p -> {
-                String markdownForHomework = getMarkdownParticipant(p);
-                writer.print(markdownForHomework);
-            });
+    private Participant findParticipant(String username, List<Participant> participants) {
+        boolean isNewUser = participants.stream().noneMatch(p -> p.username().equals(username));
+        Participant participant = null;
+        if (isNewUser) {
+            participant = new Participant(username);
+            participants.add(participant);
+        } else {
+            participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
         }
+        return participant;
     }
 
-    private String getMarkdownParticipant(Participant p) {
-        return String.format("| %s %s | %.2f%% |\n",
-                             p.username(),
-                             checkMark(p),
-                             p.getRate(this.totalNumberOfEvents));
-    }
-
-    /**
-     * | 참여자 (420) | 1주차 | 2주차 | 3주차 | 참석율 |
-     * | --- | --- | --- | --- | --- |
-     */
-    private String header(int totalNumberOfParticipants) {
-        StringBuilder header = new StringBuilder(String.format("| 참여자 (%d) |", totalNumberOfParticipants));
-
-        for (int index = 1; index <= this.totalNumberOfEvents; index++) {
-            header.append(String.format(" %d주차 |", index));
-        }
-        header.append(" 참석율 |\n");
-
-        header.append("| --- ".repeat(Math.max(0, this.totalNumberOfEvents + 2)));
-        header.append("|\n");
-
-        return header.toString();
-    }
-
-    /**
-     * |:white_check_mark:|:white_check_mark:|:white_check_mark:|:x:|
-     */
-    private String checkMark(Participant p) {
-        StringBuilder line = new StringBuilder();
-        for (int i = 1 ; i <= this.totalNumberOfEvents ; i++) {
-            if(p.homework().containsKey(i) && p.homework().get(i)) {
-                line.append("|:white_check_mark:");
-            } else {
-                line.append("|:x:");
-            }
-        }
-        return line.toString();
-    }
 }
