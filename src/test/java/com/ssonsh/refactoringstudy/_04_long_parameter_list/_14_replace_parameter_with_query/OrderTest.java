a0b5fc01a462bdf9d69f74100d8e5b954ac941a8
ssh1224@midasin.com <ssh1224@midasin.com>
2022-03-25 06:55:01 +0900

    리팩토링 - 긴 매개변수 목록
    - 매개변수를 질의 함수로 바꾸기

---
+++ src/test/java/com/ssonsh/refactoringstudy/_04_long_parameter_list/_14_replace_parameter_with_query/OrderTest.java
@@ -0,0 +1,24 @@
+package com.ssonsh.refactoringstudy._04_long_parameter_list._14_replace_parameter_with_query;
+
+import com.ssonsh.refactoringstudy._04_long_parameter.Order;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OrderTest {
+
+    @Test
+    void discountedPriceWithDiscountLevel2() {
+        int quantity = 200;
+        double price = 100;
+        assertEquals(quantity * price * 0.90, new Order(quantity, price).finalPrice());
+    }
+
+    @Test
+    void discountedPriceWithDiscountLevel1() {
+        int quantity = 100;
+        double price = 100;
+        assertEquals(quantity * price * 0.95, new Order(quantity, price).finalPrice());
+    }
+
+}
---
+++ src/test/java/com/ssonsh/refactoringstudy/_04_long_parameter_list/_15_remove_flag_argument/ShipmentTest.java
@@ -0,0 +1,22 @@
+package com.ssonsh.refactoringstudy._04_long_parameter_list._15_remove_flag_argument;
+
+import com.ssonsh.refactoringstudy._04_long_parameter.Order;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+class ShipmentTest {
+
+    @Test
+    void deliveryDate() {
+        /*
+        LocalDate placedOn = LocalDate.of(2021, 12, 15);
+        Order orderFromWA = new Order(placedOn, "WA");
+
+        Shipment shipment = new Shipment();
+        assertEquals(placedOn.plusDays(1), shipment.deliveryDate(orderFromWA, true));
+        assertEquals(placedOn.plusDays(2), shipment.deliveryDate(orderFromWA, false));
+         */
+    }
+
+}
