ac7b8edef5dd199fda0bd2ba76df041de5741b6a
ssh1224@midasin.com <ssh1224@midasin.com>
2022-03-28 07:50:45 +0900

    리팩토링 - 가변 데이터
    - 변수 쪼개기 - 1

---
+++ src/main/java/com/ssonsh/refactoringstudy/_06_mutable_data/_18_split_variable/Rectangle.java
@@ -0,0 +1,25 @@
+package com.ssonsh.refactoringstudy._06_mutable_data._18_split_variable;
+
+public class Rectangle {
+
+    private double perimeter;
+    private double area;
+
+    public void updateGeometry(double height, double width) {
+        double perimeter = 2 * (height + width);
+        System.out.println("Perimeter: " + perimeter);
+        this.perimeter = perimeter;
+
+        double area = height * width;
+        System.out.println("Area: " + area);
+        this.area = area;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+
+    public double getArea() {
+        return area;
+    }
+}
---
+++ src/test/java/com/ssonsh/refactoringstudy/_18_split_variable/HaggisTest.java
@@ -0,0 +1,19 @@
+package com.ssonsh.refactoringstudy._18_split_variable;
+
+import com.ssonsh.refactoringstudy._06_mutable_data._18_split_variable.Haggis;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HaggisTest {
+
+    @Test
+    void distance() {
+        /*
+        Haggis haggis = new Haggis(10d, 20d, 10, 5);
+        assertEquals(50d, haggis.distanceTravelled(10));
+        assertEquals(125d, haggis.distanceTravelled(20));
+        */
+    }
+
+}
