53961fe006276069deab227f570de6da1e619d85
ssonsh <chosunci@gmail.com>
2022-03-28 08:31:27 +0900

    리팩토링 - 가변 데이터
    - 계정.. ㅠ

--- src/test/java/com/ssonsh/refactoringstudy/_18_split_variable/OrderTest.java
+++ src/test/java/com/ssonsh/refactoringstudy/_18_split_variable/OrderTest.java
@@ -1,18 +1,19 @@
 package com.ssonsh.refactoringstudy._18_split_variable;
 
+import com.ssonsh.refactoringstudy._06_mutable_data._18_split_variable.Order;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OrderTest {
 
     @Test
     void discount() {
-        /*
         Order order = new Order();
         assertEquals(50d, order.discount(50d, 100));
         assertEquals(51d - 2, order.discount(51d, 100));
         assertEquals(50d - 1, order.discount(50d, 101));
         assertEquals(51d - 2 - 1, order.discount(51d, 101));
-        */
     }
 
 }
