b7b49f5c3f9c652281c9116cfd30bcab7e2c57e5
ssh1224@midasin.com <ssh1224@midasin.com>
2022-03-28 08:02:03 +0900

    리팩토링 - 가변 데이터
    - 변수 쪼개기 - 2

--- src/test/java/com/ssonsh/refactoringstudy/_18_split_variable/HaggisTest.java
+++ src/test/java/com/ssonsh/refactoringstudy/_18_split_variable/HaggisTest.java
@@ -9,11 +9,9 @@ class HaggisTest {
 
     @Test
     void distance() {
-        /*
         Haggis haggis = new Haggis(10d, 20d, 10, 5);
         assertEquals(50d, haggis.distanceTravelled(10));
         assertEquals(125d, haggis.distanceTravelled(20));
-        */
     }
 
 }
