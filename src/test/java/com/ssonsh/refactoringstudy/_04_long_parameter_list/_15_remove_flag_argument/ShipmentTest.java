d399e1b47632514464bf4ddfb5bc7130c8dd42ef
ssh1224@midasin.com <ssh1224@midasin.com>
2022-03-28 06:46:47 +0900

    리팩토링 - 긴 매개변수 목록
    - 플래그 인수 제거하기

--- src/test/java/com/ssonsh/refactoringstudy/_04_long_parameter_list/_15_remove_flag_argument/ShipmentTest.java
+++ src/test/java/com/ssonsh/refactoringstudy/_04_long_parameter_list/_15_remove_flag_argument/ShipmentTest.java
@@ -1,22 +1,23 @@
 package com.ssonsh.refactoringstudy._04_long_parameter_list._15_remove_flag_argument;
 
-import com.ssonsh.refactoringstudy._04_long_parameter.Order;
+import com.ssonsh.refactoringstudy._04_long_parameter._15_remove_flag_argument.Order;
+import com.ssonsh.refactoringstudy._04_long_parameter._15_remove_flag_argument.Shipment;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ShipmentTest {
 
     @Test
     void deliveryDate() {
-        /*
         LocalDate placedOn = LocalDate.of(2021, 12, 15);
         Order orderFromWA = new Order(placedOn, "WA");
 
         Shipment shipment = new Shipment();
-        assertEquals(placedOn.plusDays(1), shipment.deliveryDate(orderFromWA, true));
-        assertEquals(placedOn.plusDays(2), shipment.deliveryDate(orderFromWA, false));
-         */
+        assertEquals(placedOn.plusDays(1), shipment.rushDeliveryDate(orderFromWA));
+        assertEquals(placedOn.plusDays(2), shipment.regularDeliveryDate(orderFromWA));
     }
 
 }
