58e024c5e35171e2f744f569b8d778c2d2ffda09
ssonsh <chosunci@gmail.com>
2022-03-31 07:25:11 +0900

    리팩토링 - 기능 편애

---
+++ src/main/java/com/ssonsh/refactoringstudy/_09_feature_envy/ElectricityUsage.java
@@ -0,0 +1,27 @@
+package com.ssonsh.refactoringstudy._09_feature_envy;
+
+public class ElectricityUsage {
+
+    private double amount;
+
+    private double pricePerUnit;
+
+    public ElectricityUsage(double amount, double pricePerUnit) {
+        this.amount = amount;
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public double getPricePerUnit() {
+        return pricePerUnit;
+    }
+
+
+    public double getElecticityBill() {
+        return this.getAmount() * this.getPricePerUnit();
+    }
+
+}
---
+++ src/main/java/com/ssonsh/refactoringstudy/_09_feature_envy/package-info.java
@@ -0,0 +1,1 @@
+package com.ssonsh.refactoringstudy._09_feature_envy;
