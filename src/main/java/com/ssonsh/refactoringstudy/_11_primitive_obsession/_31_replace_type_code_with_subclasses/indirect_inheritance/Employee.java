0f030f801aa216787613e6987304c2401251e756
ssonsh <chosunci@gmail.com>
2022-04-04 07:05:16 +0900

    리팩토링 - 기본형 집착
    - 타입 코드를 서브클래스로 바꾸기

---
+++ src/main/java/com/ssonsh/refactoringstudy/_11_primitive_obsession/_31_replace_type_code_with_subclasses/indirect_inheritance/Employee.java
@@ -0,0 +1,30 @@
+package com.ssonsh.refactoringstudy._11_primitive_obsession._31_replace_type_code_with_subclasses.indirect_inheritance;
+
+public class Employee {
+
+    private String name;
+
+    private EmployeeType type;
+
+    public Employee(String name, String typeValue) {
+        this.name = name;
+        this.type = this.employeeType(typeValue);
+    }
+
+    private EmployeeType employeeType(String typeValue) {
+        return switch (typeValue) {
+            case "engineer" -> new Engineer();
+            case "manager" -> new Manager();
+            case "salesman" -> new Salesman();
+            default -> throw new IllegalArgumentException();
+        };
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+               "name='" + name + '\'' +
+               ", type='" + this.type.toString() + '\'' +
+               '}';
+    }
+}
---
+++ src/main/java/com/ssonsh/refactoringstudy/_11_primitive_obsession/_31_replace_type_code_with_subclasses/package-info.java
@@ -0,0 +1,1 @@
+package com.ssonsh.refactoringstudy._11_primitive_obsession._31_replace_type_code_with_subclasses;
